44759d23b6169e8b452f500110d225579f31f632
satojin <seiron1212@outlook.jp>
2022-02-10 21:28:17 +0900

    ヘッダーの切り替え機能の実装

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { UserData } from "globalType";
+import { FaCalendarAlt, FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import {
+  BsFillBrightnessAltHighFill,
+  BsFillSunFill,
+  BsFillMoonFill,
+} from "react-icons/bs";
+import { RiArrowGoBackFill } from "react-icons/ri";
+import { MdFastfood } from "react-icons/md";
+type Props = {
+  meal: string;
+  isEdit: boolean;
+};
 
-import React, { useState } from 'react'
-import { UserData } from 'globalType'
-import {FaCalendarAlt,FaAngleLeft, FaAngleRight} from 'react-icons/fa'
+export const Header: React.VFC<Props> = (props) => {
+  let headerColor!: string;
+  let headerIcon!: JSX.Element | null;
+  (function(){
+    if (props.meal == "breakfast") {
+      headerColor = "bg-red-500";
+      headerIcon = (
+        <div className="text-3xl flex">
+          <BsFillBrightnessAltHighFill size={40} className="mr-2" />
+          <p>朝食</p>
+        </div>
+      );
+      return;
+    } else if (props.meal == "lunch") {
+      headerColor = "bg-yellow-400";
+      headerIcon = (
+        <div className="text-3xl flex">
+          <BsFillSunFill size={40} className="mr-2" />
+          <p>昼食</p>
+        </div>
+      );
+      return;
+    } else if (props.meal == "dinner") {
+      headerColor = "bg-purple-500";
+      headerIcon =  (
+        <div className="text-3xl flex">
+          <BsFillMoonFill size={40} className="mr-2" />
+          <p>夕食</p>
+        </div>
+      );
+      return;
+    } else if (props.meal == "snack") {
+      headerColor = "bg-sky-400";
+      headerIcon = (
+        <div className="text-3xl flex">
+          <MdFastfood size={40} className="mr-2" />
+          <p>間食</p>
+        </div>
+      );
+      return;
+    } else {
+      headerColor = "bg-orange-500";
+      headerIcon = <div></div>;
+      return; 
+    }
+  }());
 
-export const Header :React.VFC = () => {
 
   return (
     <div>
-      <header className="bg-orange-500">
+      <header className={headerColor}>
         <div className="flex justify-around text-white font-bold py-5">
-          <div></div>
+          {headerIcon}
           <div className="flex items-center  font-bold text-3xl ">
-            <button className='cursor-pointer mr-2 font-bold'><FaAngleLeft/></button>
-            <h1 className='text-2xl'> 2022 / 01 / 22 (土) </h1>
-            <button className=' ml-2 font-bold'><FaAngleRight/></button>
+            {!props.isEdit ? (
+              <button className="cursor-pointer mr-2 font-bold">
+                <FaAngleLeft />
+              </button>
+            ) : null}
+
+            <h1 className="text-2xl"> 2022 / 01 / 22 (土) </h1>
+            {!props.isEdit ? (
+              <button className=" ml-2 font-bold">
+                <FaAngleRight />
+              </button>
+            ) : null}
           </div>
-          <button>
-          <FaCalendarAlt size={40}/>
-          </button>
+          {!props.isEdit ? (
+            <button>
+              <FaCalendarAlt size={40} />
+            </button>
+          ) : (
+            <button>
+              <RiArrowGoBackFill size={40} />
+            </button>
+          )}
         </div>
       </header>
-
     </div>
-  )
-}
+  );
+};
--- components/NutritionList.tsx
+++ components/NutritionList.tsx
@@ -6,7 +6,7 @@ type Props = {
   nutrition: Nutrition | undefined;
 };
 export const NutritionList: React.VFC<Props> = (props) => {
-  let [isAllShow,setIsAllShow] = useState<boolean>(false);
+  let [isAllShow, setIsAllShow] = useState<boolean>(false);
   const nutritionItems =
     props.nutrition != undefined
       ? Object.keys(props.nutrition).map((key: string, index: number) => {
@@ -22,33 +22,36 @@ export const NutritionList: React.VFC<Props> = (props) => {
         })
       : null;
 
-      const toggleDisableNutrtion = ()=>{
-        const target = document.getElementsByClassName("disable");
-        if(isAllShow){
-          target[0].className =
-            "disable md:h-24 h-48 text-clip overflow-hidden md:flex flex-row flex-wrap";
-          setIsAllShow(false);
-        }else{
-          target[0].className = "disable  md:flex flex-row flex-wrap";
-          setIsAllShow(true);
-
-        }
-         
-      }
+  const toggleDisableNutrtion = () => {
+    const target = document.getElementsByClassName("disable");
+    if (isAllShow) {
+      target[0].className =
+        "disable md:h-24 h-48 text-clip overflow-hidden md:flex flex-row flex-wrap";
+      setIsAllShow(false);
+    } else {
+      target[0].className = "disable  md:flex flex-row flex-wrap";
+      setIsAllShow(true);
+    }
+  };
 
   return (
     <div className="border-2 border-red-700 rounded mt-10 sms:mx-20 p-4 bg-white">
-      {props.nutrition != undefined ?
-      <div className="disable md:h-24 h-48 text-clip overflow-hidden md:flex flex-row flex-wrap">
-        {nutritionItems}
-      </div> :null
-}
-      <div className=" mt-5 flex justify-end">
-        <button onClick={toggleDisableNutrtion} className="text-md font-bold">
-          {isAllShow ? "戻す" : "全ての栄養素を見る"}
-          <span className="text-red-500">&gt;</span>
-        </button>
-      </div>
+      {props.nutrition != undefined ? (
+        <div>
+          <div className="disable md:h-24 h-48 text-clip overflow-hidden md:flex flex-row flex-wrap">
+            {nutritionItems}
+          </div>
+          <div className=" mt-5 flex justify-end">
+            <button
+              onClick={toggleDisableNutrtion}
+              className="text-md font-bold"
+            >
+              {isAllShow ? "戻す" : "全ての栄養素を見る"}
+              <span className="text-red-500">&gt;</span>
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
