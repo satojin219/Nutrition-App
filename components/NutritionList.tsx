a451547316715c15b4178c43b9010a9c9cb4cba7
satojin <seiron1212@outlook.jp>
2022-02-10 11:30:02 +0900

    栄養表示の切り替え

--- components/NutritionList.tsx
+++ components/NutritionList.tsx
@@ -1,11 +1,12 @@
 import { Nutrition } from "globalType";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { extractNutrition } from "../tools/HelpMethods";
 
 type Props = {
   nutrition: Nutrition | undefined;
 };
 export const NutritionList: React.VFC<Props> = (props) => {
+  let [isAllShow,setIsAllShow] = useState<boolean>(false);
   const nutritionItems =
     props.nutrition != undefined
       ? Object.keys(props.nutrition).map((key: string, index: number) => {
@@ -21,13 +22,31 @@ export const NutritionList: React.VFC<Props> = (props) => {
         })
       : null;
 
+      const toggleDisableNutrtion = ()=>{
+        const target = document.getElementsByClassName("disable");
+        if(isAllShow){
+          target[0].className =
+            "disable md:h-24 h-48 text-clip overflow-hidden md:flex flex-row flex-wrap";
+          setIsAllShow(false);
+        }else{
+          target[0].className = "disable  md:flex flex-row flex-wrap";
+          setIsAllShow(true);
+
+        }
+         
+      }
+
   return (
-    <div className="border-2 border-red-700 rounded mt-10 sms:mx-20 p-4  bg-white">
-      <div className=" md:flex flex-row flex-wrap">{nutritionItems}</div>
-      <div className="flex justify-end">
-        <button className="text-md font-bold">
-          {" "}
-          詳しく見る <span className="text-red-500">&gt;</span>
+    <div className="border-2 border-red-700 rounded mt-10 sms:mx-20 p-4 bg-white">
+      {props.nutrition != undefined ?
+      <div className="disable md:h-24 h-48 text-clip overflow-hidden md:flex flex-row flex-wrap">
+        {nutritionItems}
+      </div> :null
+}
+      <div className=" mt-5 flex justify-end">
+        <button onClick={toggleDisableNutrtion} className="text-md font-bold">
+          {isAllShow ? "戻す" : "全ての栄養素を見る"}
+          <span className="text-red-500">&gt;</span>
         </button>
       </div>
     </div>
