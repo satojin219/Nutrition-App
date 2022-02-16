c9cd954a7efb21e92ebf64495a828cc8f578c3a0
satojin <seiron1212@outlook.jp>
2022-02-16 10:51:13 +0900

    きせさんのレビュー修正

--- components/editMenu/FoodImage.tsx
+++ components/editMenu/FoodImage.tsx
@@ -10,20 +10,17 @@ export const FoodImage: React.VFC = () => {
     <div className="basis-1/3 md:basis-1/3 text-xl bg-gray-400 text-white ">
       <div className=" flex justify-center items-center ">
         {preview != "" ? (
-          <img
-            src={preview}
-            className="object-fill w-full h-full bg-cover bg-contain"
-          />
+          <img src={preview} className="object-fill w-full h-full" />
         ) : (
           <div className="flex justify-center items-center h-48">
-            <img src="" alt="No image" className="" />
+            <img src="" alt="No image" />
           </div>
         )}
       </div>
 
       <div className="relative">
         <label className="cursor-pointer text-center bg-zinc-600 text-white p-2 rounded-full  shadow-md w-13 font-bold mr-4 mb-3 absolute  bottom-0 right-0 text-4xl">
-          {preview != "" ? <MdFlipCameraIos /> : <MdAddAPhoto />}
+          {preview == "" ? <MdAddAPhoto /> : <MdFlipCameraIos />}
           <input
             type="file"
             name="photo"
