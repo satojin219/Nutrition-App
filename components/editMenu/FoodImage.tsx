c66bd6e479b7b09a31efe1e429bf52728bf7a125
satojin <seiron1212@outlook.jp>
2022-03-02 11:52:13 +0900

    Imageタグに必要なプロパティの追加

--- components/editMenu/FoodImage.tsx
+++ components/editMenu/FoodImage.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { MdAddAPhoto, MdFlipCameraIos } from "react-icons/md";
 export const FoodImage: React.VFC = () => {
-  const [preview, setPreview] = useState("");
+  const [preview, setPreview] = useState("/public/m_e_others_501.png");
   const handleChangeFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const { files } = e.currentTarget;
     setPreview(window.URL.createObjectURL(files![0]));
   };
+
   return (
-    <div className="basis-1/3 md:basis-1/3 text-xl bg-gray-400 text-white ">
-      <div className="flex justify-center items-center">
-        {preview != "" ? (
-          <Image
-            src={preview}
-            className="object-fill w-full h-full"
-            alt="preview"
-          />
-        ) : (
-          <div className="flex justify-center items-center h-48">
-            <Image src="" alt="No image" />
-          </div>
-        )}
-      </div>
+    <div className="basis-1/3 md:basis-1/3 text-xl text-white">
+      <Image
+        src={preview}
+        className="text-center"
+        alt="No Image"
+        height={300}
+        width={500}
+        objectFit={"contain"}
+      />
 
       <div className="relative">
         <label className="cursor-pointer text-center bg-zinc-600 text-white p-2 rounded-full shadow-md w-13 font-bold mr-4 mb-3 absolute bottom-0 right-0 text-4xl">
-          {preview == "" ? <MdAddAPhoto /> : <MdFlipCameraIos />}
+          {preview == "/public/m_e_others_501.png" ? (
+            <MdAddAPhoto />
+          ) : (
+            <MdFlipCameraIos />
+          )}
           <input
             type="file"
             name="photo"
