cc4ae3ab7564cd8076eda56a12059e0e1bec99e7
satojin <seiron1212@outlook.jp>
2022-03-02 19:50:14 +0900

    画像挿入ボタンの場合分けの修正

--- components/editMenu/FoodImage.tsx
+++ components/editMenu/FoodImage.tsx
@@ -21,7 +21,7 @@ export const FoodImage: React.VFC = () => {
 
       <div className="relative">
         <label className="cursor-pointer text-center bg-zinc-600 text-white p-2 rounded-full shadow-md w-13 font-bold mr-4 mb-3 absolute bottom-0 right-0 text-4xl">
-          {preview == "/public/m_e_others_501.png" ? (
+          {preview == "/../public/m_e_others_501.png" ? (
             <MdAddAPhoto />
           ) : (
             <MdFlipCameraIos />
