a116f27153c2e58e6b4b06bdc630549de4848a34
satojin <seiron1212@outlook.jp>
2022-03-02 20:12:26 +0900

    pathの修正

--- components/editMenu/FoodImage.tsx
+++ components/editMenu/FoodImage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { MdAddAPhoto, MdFlipCameraIos } from "react-icons/md";
 export const FoodImage: React.VFC = () => {
-  const [preview, setPreview] = useState("/../public/m_e_others_501.png");
+  const [preview, setPreview] = useState("/m_e_others_501.png");
   const handleChangeFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const { files } = e.currentTarget;
     setPreview(window.URL.createObjectURL(files![0]));
@@ -21,7 +21,7 @@ export const FoodImage: React.VFC = () => {
 
       <div className="relative">
         <label className="cursor-pointer text-center bg-zinc-600 text-white p-2 rounded-full shadow-md w-13 font-bold mr-4 mb-3 absolute bottom-0 right-0 text-4xl">
-          {preview == "/../public/m_e_others_501.png" ? (
+          {preview == "/m_e_others_501.png" ? (
             <MdAddAPhoto />
           ) : (
             <MdFlipCameraIos />
