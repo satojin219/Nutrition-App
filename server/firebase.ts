318e87a856fd31e996b57cc61bbaea88facf651b
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-03-10 13:29:18 +0900

    fix rename and add comment

--- server/firebase.ts
+++ server/firebase.ts
@@ -4,11 +4,11 @@ import { getFirestore } from "firebase-admin/firestore";
 console.log("app1", getApps());
 
 if (getApps().length === 0) {
-  const c = JSON.parse(process.env.FIREBASE_CREDENTIAL as string);
+  const credEscaped = JSON.parse(process.env.FIREBASE_CREDENTIAL as string);
   initializeApp({
     credential: cert({
-      ...c,
-      privateKey: c.private_key.replace(/\\n/g, "\n"),
+      ...credEscaped,
+      privateKey: credEscaped.private_key.replace(/\\n/g, "\n"), // PEMの改行文字がJSONから文字列にするときにエスケープされてしまう。エスケープをここで解除する。
     }),
   });
 }
