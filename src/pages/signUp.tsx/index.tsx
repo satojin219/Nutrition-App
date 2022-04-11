fb3e43591db25f1365aa371b64cc30965040a644
satojin <seiron1212@outlook.jp>
2022-04-11 19:16:53 +0900

    UI完成

---
+++ src/pages/signUp.tsx/index.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { app } from "../../server/firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const SignUp: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nutriton App</title>
+      </Head>
+      <div className="bg-orange-500 flex justify-center items-center h-screen flex-col">
+        <h1 className="text-3xl text-white mb-10">Nutrition App</h1>
+        <form className="bg-white p-5 rounded-md w-1/2">
+          <div className="flex flex-col my-3">
+            <label>メールアドレス</label>
+            <input
+              className="border-yellow-700/50 border-b-2 my-2"
+              name="email"
+              type="email"
+              placeholder=""
+              required
+            />
+          </div>
+          <div className="flex flex-col my-3">
+            <label>パスワード</label>
+            <input
+              className="border-yellow-700/50 border-b-2 my-2"
+              name="password"
+              type="password"
+              required
+            />
+          </div>
+          <div>
+            <div className="flex justify-end">
+              <p className="text-xs">
+                <a className="text-blue-400">ログイン</a>はこちらから
+              </p>
+            </div>
+            <div className="flex justify-center ">
+              <button className="my-3 w-full bg-orange-500 text-white px-3 py-2 rounded-md">
+                登録
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -1,9 +1,10 @@
 import { getApps, cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+export let app: any;
 if (getApps().length === 0) {
   const credEscaped = JSON.parse(process.env.FIREBASE_CREDENTIAL as string);
-  initializeApp({
+  app = initializeApp({
     credential: cert({
       ...credEscaped,
       privateKey: credEscaped.private_key.replace(/\\n/g, "\n"), // PEMの改行文字がJSONから文字列にするときにエスケープされてしまう。エスケープをここで解除する。
